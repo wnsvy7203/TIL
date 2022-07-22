b669375eb5e871323731fcc8a94288b3e3cfa08c
wnsvy7203 <wnsvy7203@gmail.com>
2022-07-22 22:33:29 +0900

    0722 01_pjt 제출

--- 0722/pjt/02_students-20220722T000036Z-001/02_students/problem_e.py
+++ 0722/pjt/02_students-20220722T000036Z-001/02_students/problem_e.py
@@ -2,23 +2,17 @@ import json
 
 
 def dec_movies(movies):
-    a = []
     r = []
+
     for i in range(len(movies)):
-        # id 에 맞게 1대 1 load
         idm = open('data/movies/'+str(movies[i].get("id"))+'.json', encoding='utf-8')
-        idm2 = json.load(idm) 
-        a.append(idm2)
-        r.append(idm2["release_date"])
-
-        n = 0
-        for j in range(len(r)):
-            if r[j][5] == 1 and r[j][6] == 2:
-                n = j
+        idm2 = json.load(idm)
+        # id 에 맞게 1대 1 load
         
-        title = a[n].get("title")
+        if idm2["release_date"][5] == '1' and idm2["release_date"][6] == '2':
+            r.append(idm2["title"])
 
-    return title
+    return r
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
