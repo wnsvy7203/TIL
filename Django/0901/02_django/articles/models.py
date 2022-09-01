3cb652922fe5c859a8a1565c92b02c7a69bba4ae
wnsvy7203 <wnsvy7203@gmail.com>
2022-09-01 18:01:06 +0900

    0901

--- Django/0901/02_django/articles/models.py
+++ Django/0901/02_django/articles/models.py
@@ -7,6 +7,5 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.title
--- Django/0901/02_django/crud/urls.py
+++ Django/0901/02_django/crud/urls.py
@@ -1,4 +1,4 @@
-"""project1 URL Configuration
+"""crud URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from fruits import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fruits/', views.fruit),
+    path('articles/', include('articles.urls')),
 ]
