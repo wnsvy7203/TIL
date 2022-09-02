ef42ddb762398b1e37d55b34638519adf89c4adc
wnsvy7203 <wnsvy7203@gmail.com>
2022-09-02 17:45:33 +0900

    0902

---
+++ BOJ/02_실버/11866_요세푸스_문제_0.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+N, K = map(int, input().split())
+arr = deque(range(1, N+1))
+new_arr = []
+while arr:
+    arr.rotate(-(K-1))
+    new_arr.append(str(arr.popleft()))
+result = ', '.join(new_arr)
+print(f"<{', '.join(new_arr)}>")
+
+
