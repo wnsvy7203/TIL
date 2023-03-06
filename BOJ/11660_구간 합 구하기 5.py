f444a6d31bccf8069c9eb6e8673689d74fa6e39b
wnsvy7203 <wnsvy7203@gmail.com>
2023-03-06 18:01:42 +0900

    03061801

---
+++ BOJ/11660_구간 합 구하기 5.py
@@ -0,0 +1,19 @@
+# silver 1
+# 0ms
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(N):
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+
+    print()
+
+
+
