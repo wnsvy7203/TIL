a1e62231cad25587b5f77aeee2f69bd6cbbad935
wnsvy7203 <wnsvy7203@gmail.com>
2023-05-15 02:09:52 +0900

    05150209

--- CPP/BOJ/Bronze/2903.cpp
+++ CPP/BOJ/Bronze/2903.cpp
@@ -8,18 +8,8 @@ using namespace std;
 int main()
 {
     int N;
-    int x = 2;
 
     cin >> N;
 
     cout << (int)pow((pow(2, N) + 1), 2);
-
-    while (N--)
-    {
-        x += x - 1;
-    }
-
-    x *= x;
-
-    cout << x;
 }
